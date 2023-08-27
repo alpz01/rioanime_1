5081ef41113f7d17d59b268c3eb619ddf33d3376
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-08-27 11:27:08 +0800

    Update and rename main14.js to main15.js

--- main15.js
+++ main15.js
@@ -147,7 +147,7 @@ function PlayerSection() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"></path>
                         </svg>
                     </a>
-                    <span className="animetitle">Love Live! Superstar!! 2nd Season</span>
+                    <span className="animetitle">{"<data:post.title/>"}</span>
                     <button id="trackbtn" onClick={startTrack}>
                         <i className="glyphicon glyphicon-plus"></i> Watchlist
                     </button>
