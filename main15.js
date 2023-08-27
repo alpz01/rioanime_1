6d525af43ce0b5040583f463e8a176ecfdae21a3
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-08-27 11:29:09 +0800

    Update main16.js

--- main15.js
+++ main15.js
@@ -88,6 +88,8 @@ function PlayerSection() {
         }
     }
 
+    let postTitle = "<data:post.title/>";
+
     return (
         <div className="playerpage">
             <div className="subpart eptitle">
@@ -147,7 +149,7 @@ function PlayerSection() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"></path>
                         </svg>
                     </a>
-                    <span className="animetitle">{"<data:post.title/>"}</span>
+                    <span className="animetitle">{postTitle}</span>
                     <button id="trackbtn" onClick={startTrack}>
                         <i className="glyphicon glyphicon-plus"></i> Watchlist
                     </button>
