424cb358e27e11a19c244e4ae1a40c2559ca3fc6
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-08-30 14:51:06 +0800

    Update and rename home_5.jsx to home_6.jsx

--- home_6.jsx
+++ home_6.jsx
@@ -17,8 +17,9 @@ function GeneratePost() {
     }, []);
 
     return (
-        <div>
+        <>
             {data.map((post, index) => {
+                const key = index.toString();
                 let title = post.title.$t;
                 let score = -1;
                 let ep = "No Item";
@@ -48,33 +49,32 @@ function GeneratePost() {
                 }
 
                 return (
-                    <>
-                        <div key={index} className="hentry play c:hover-eee">
-                            <a className="block ofc relative poster r3 oh" href={postLink} title={title}>
-                                <img alt={title} className="ar-2sx h-max w-max" loading="lazy" src={imageLink} />
-                                <div className="absolute b-0 p-y2x6b0 ep fs-13 c-eee blr5 trr8">
-                                    <span>{ep}</span>
-                                </div>
-                                <div className="absolute t-0 p-y2x6b0 sc fs-13 c-eee tlr5 brr8">
-                                    <div className="rating-prc">
-                                        <div className="rtp">
-                                            <div className="rtb"><span style={{ width: `${score * 10}%` }}></span></div>
-                                        </div>
-                                        <div className="num" content={score}>{score}</div>
+                    <div key={key} className="hentry play c:hover-eee">
+                        <a className="block ofc relative poster r3 oh" href={postLink} title={title}>
+                            <img alt={title} className="ar-2sx h-max w-max" loading="lazy" src={imageLink} />
+                            <div className="absolute b-0 p-y2x6b0 ep fs-13 c-eee blr5 trr8">
+                                <span>{ep}</span>
+                            </div>
+                            <div className="absolute t-0 p-y2x6b0 sc fs-13 c-eee tlr5 brr8">
+                                <div className="rating-prc">
+                                    <div className="rtp">
+                                        <div className="rtb"><span style={{ width: `${score * 10}%` }}></span></div>
                                     </div>
+                                    <div className="num" content={score}>{score}</div>
                                 </div>
-                                <div className="absolute b-0 r-0 fs-13 c-eee brr5 tlr8 dir ttu">
-                                    {type && <span className={type.toLowerCase()}>{type}</span>}
-                                    {view && <span className={view.toLowerCase()}>{view}</span>}
-                                </div>
-                            </a>
-                        </div>
-                    </>
+                            </div>
+                            <div className="absolute b-0 r-0 fs-13 c-eee brr5 tlr8 dir ttu">
+                                {type && <span className={type.toLowerCase()}>{type}</span>}
+                                {view && <span className={view.toLowerCase()}>{view}</span>}
+                            </div>
+                        </a>
+                    </div>
                 );
             })}
-        </div>
+        </>
     );
 }
+
 
 function PostMenu() {
     return (
