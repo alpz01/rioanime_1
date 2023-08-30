39cf9bb975d308de0c15729b815210c90d46ff16
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-08-30 20:50:04 +0800

    Update and rename home_12.jsx to home_13.jsx

--- home_13.jsx
+++ home_13.jsx
@@ -1,9 +1,22 @@
+const postSub = () => {
+    const url = "http://www.dev-testing-website.blogspot.com/feeds/posts/default/-/sub?alt=json";
+    React.useEffect(() => {
+        axios.get(url)
+            .then(data => {
+                console.log(data.feed.entry);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    })
+}
+
 const GeneratePost = () => {
     const [data, setData] = React.useState([]);
+    const url = "https://dev-testing-website.blogspot.com/feeds/posts/default?alt=json&max-results=25";
+    const storedData = localStorage.getItem("pppDatapostrr");
 
     React.useEffect(() => {
-        const url = "https://dev-testing-website.blogspot.com/feeds/posts/default?alt=json&max-results=25";
-        const storedData = localStorage.getItem("pppDatapostrr");
         if (storedData) {
             setData(JSON.parse(storedData));
         } else {
@@ -17,9 +30,8 @@ const GeneratePost = () => {
                 });
         }
     }, []);
-
+    
     React.useEffect(() => {
-        const url = "https://dev-testing-website.blogspot.com/feeds/posts/default?alt=json&max-results=25";
         axios.get(url)
             .then(response => {
                 const newData = response.data.feed.entry;
@@ -32,41 +44,41 @@ const GeneratePost = () => {
                 console.error('Error:', error);
             });
     }, [data]);
-
+    
     return (
         <>
             {data.map((post, index) => {
                 const key = index.toString();
-                const { title, link, category, content } = post;
+                let title = post.title.$t;
                 let score = -1;
                 let ep = "No Item";
                 let type = "No Item";
                 let view = "No Item";
-                let postLink = link[4].href;
+                let postLink = post.link[4].href;
                 let imageLink = "No Item";
 
-                category.forEach(category => {
+                post.category.forEach(category => {
                     const term = category.term;
-                    if (term === "Sub" || term === "Dub") {
+                    if (term == "Sub" || term == "Dub") {
                         type = term;
                     } else if (term.startsWith("Ep")) {
                         ep = term;
                     } else if (!isNaN(term)) {
                         score = Number(term);
-                    } else if (term === "Movie" || term === "Ova" || term === "TV") {
+                    } else if (term == "Movie" || term == "Ova" || term == "TV") {
                         view = term;
                     }
                 });
 
-                const images = content?.$t.match(/<img[^>]+src="([^">]+)/i);
-                if (images && images.length > 1) {
-                    imageLink = images[1];
-                }
+                const content = post.content.$t;
+                const regex = /<img[^>]+src=["']([^"']+)["'][^>]*>/;
+                const match = content.match(regex);
+                imageLink = match ? match[1] : null;
 
                 return (
                     <div key={key} className="hentry play c:hover-eee">
-                        <a className="block ofc relative poster r3 oh" href={postLink} title={title.$t}>
-                            <img alt={title.$t} className="ar-2sx h-max w-max" loading="lazy" src={imageLink} />
+                        <a className="block ofc relative poster r3 oh" href={postLink} title={title}>
+                            <img alt={title} className="ar-2sx h-max w-max" loading="lazy" src={imageLink} />
                             <div className="absolute b-0 p-y2x6b0 ep fs-13 c-eee blr5 trr8">
                                 <span>{ep}</span>
                             </div>
@@ -88,13 +100,14 @@ const GeneratePost = () => {
             })}
         </>
     );
-};
+}
+
 
 const PostMenu = () => {
     return (
         <div>
             <a className='tablinks' href='/search'>All</a>
-            <a className='tablinks' href='/search/label/Sub'>Sub</a>
+            <a className='tablinks' onClick={postSub()}>Sub</a>
             <a className='tablinks' href='/search/label/Dub'>Dub</a>
             <a className='tablinks'>Random</a>
         </div>
