a495567c0678a72ec9ad1c0636c53e18fbbab9f1
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-09-03 19:22:24 +0800

    update

--- main93.jsx
+++ main93.jsx
@@ -242,7 +242,7 @@ function PlayerSection() {
             <div id="iframecontainer" className={sourceType === 'yt' || sourceType === 'gdrive' ? 'responYt' : ''}>
                 {sourceType === 'archive' ? (
                     <video controls="" crossOrigin="" playsInline="" poster="" id="player">
-                        <source src="" type="video/mp4" accesscontrolalloworigin />
+                        <source src="" type="video/mp4" accesscontrolalloworigin="true" />
                     </video>
                 ) : (
                     <iframe id="iframeplayer" allowFullScreen={true} scrolling="no" src="" style={{ minHeight: '0px' }}></iframe>
